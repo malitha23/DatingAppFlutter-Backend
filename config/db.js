b893deeabf7b61b799a9690106ae1bb727e68058
Malitha Sayuranga <lghmalith44@gmail.com>
2024-08-14 03:38:08 +0530

    2024-08-14

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
-var mysql      = require('mysql');
-var connection = mysql.createPool({
-    connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'lovebordnewbackend',
-  charset: 'utf8mb4'
+require('dotenv').config();
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    charset: process.env.DB_CHARSET,
 });
 
-module.exports = {connection};
+module.exports = { connection };
